3b813419b1bfdc5b7d335af40831d949601eff94
twu287 <91846668+twu287@users.noreply.github.com>
2022-04-29 12:38:36 -0500

    Update 1st version modification.
    Part1: can export jpg file but the data might be wrong
    
    Part2: can export file names but not work for the file that has been deleted.

--- runscan.c
+++ runscan.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include "ext2_fs.h"
 #include "read_ext2.h"
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
 
-void traverse_inode(off_t offset, struct ext2_inode* next_inode) {
-
-}
+char buffer [1024];
 
 int main(int argc, char **argv) {
 	if (argc != 3) {
 		printf("expected usage: ./runscan inputfile outputfile\n");
+		exit(0);
+	}
+
+	char *dir = argv[2];
+	//check if uotput directory has already existed
+	if(opendir(argv[2]) == NULL) {
+    mkdir(dir, S_IRWXU);
+	}
+	else {
+		printf("output directory already exists.\n");
 		exit(0);
 	}
 	
@@ -17,87 +28,154 @@ int main(int argc, char **argv) {
 	fd = open(argv[1], O_RDONLY);    /* open disk image */
 
 	ext2_read_init(fd);
-	printf("debug: num_groups: %d\n", num_groups); // for debug
 
 	struct ext2_super_block super;
 	struct ext2_group_desc group;
 	
 	// example read first the super-block and group-descriptor
-	// この2つは変更する必要なし
 	read_super_block(fd, 0, &super);
-	printf("1st fd: %d\n", fd);
 	read_group_desc(fd, 0, &group);
-	printf("2nd fd: %d\n", fd);
-
-	printf("koko\n");
-	printf("There are %u inodes in an inode table block and %u blocks in the inode table\n", inodes_per_block, itable_blocks);
-	//iterate the first inode block
-	off_t start_inode_table = locate_inode_table(0, &group); // offsetをinode_tableのとこに持ってく関数
 	
-	for (unsigned int i = 0; i < inodes_per_block; i++) { // inodes_per_block = 8 (1ブロックにinode:128byte いくつあるか？)
-		printf("inode %u: \n", i);
+	printf("There are %u inodes in an inode table block and %u blocks in the idnode table\n", inodes_per_block, itable_blocks);
+	//iterate the first inode block
+	off_t start_inode_table = locate_inode_table(0, &group);
+	for (unsigned int i = 0; i < inodes_per_block * itable_blocks; i++) {
 		struct ext2_inode *inode = malloc(sizeof(struct ext2_inode));
-
 		read_inode(fd, 0, start_inode_table, i, inode);
-		/* the maximum index of the i_block array should be computed from i_blocks / ((1024<<s_log_block_size)/512)* or once simplified, i_blocks/(2<<s_log_block_size)* https://www.nongnu.org/ext2-doc/ext2.html#i-blocks*/
-
+		if (inode->i_blocks == 0) continue;
+		printf("inode %u: \n", i);
+		/* the maximum index of the i_block array should be computed from i_blocks / ((1024<<s_log_block_size)/512)
+			* or once simplified, i_blocks/(2<<s_log_block_size)
+			* https://www.nongnu.org/ext2-doc/ext2.html#i-blocks
+			*/
 		unsigned int i_blocks = inode->i_blocks/(2<<super.s_log_block_size);
-		printf("number of blocks: %u, inode->i_blocks: %u\n", i_blocks, inode->i_blocks);
-		printf("Is directory? %s \n Is Regular file? %s\n", S_ISDIR(inode->i_mode) ? "true" : "false",
-					S_ISREG(inode->i_mode) ? "true" : "false");
-		printf("size: %u\n", inode->i_size);
-		
-		// print i_block numberss
-		for(unsigned int i=0; i<EXT2_N_BLOCKS; i++) { // EXT2_N_BLOCKS = 15
-			// printf("current file offset (byte): %lu\n",lseek(fd, 0, SEEK_CUR));
-			// printf("current file offset (block): %lu\n",lseek(fd, 0, SEEK_CUR)/BASE_OFFSET);
-			if (i < EXT2_NDIR_BLOCKS){                                 /* direct blocks */
-				printf("Block %2u : %u\n", i, inode->i_block[i]);
-			} else if (i == EXT2_IND_BLOCK) {                             /* single indirect block */
-				printf("Single   : %u\n", inode->i_block[i]);
-			} else if (i == EXT2_DIND_BLOCK) {                            /* double indirect block */
-				printf("Double   : %u\n", inode->i_block[i]);
-			} else if (i == EXT2_TIND_BLOCK) {                            /* triple indirect block */
-				printf("Triple   : %u\n", inode->i_block[i]);
+		printf("number of blocks %u\n", i_blocks);
+		printf("Is directory? %s \n Is Regular file? %s\n",
+			S_ISDIR(inode->i_mode) ? "true" : "false",
+			S_ISREG(inode->i_mode) ? "true" : "false");
+
+		// Part1: scan all jpg files  -> doesn't work now, the problem seems I don't seek the correct data blocks of pictures.
+		if (S_ISREG(inode->i_mode)) 
+		{
+
+		lseek(fd, (int)inode->i_block[0] * block_size, SEEK_SET);    /* position data block */
+		read(fd, buffer, block_size);    				/* read data block */
+
+		int is_jpg = 0;
+		if (buffer[0] == (char)0xff &&
+				buffer[1] == (char)0xd8 &&
+				buffer[2] == (char)0xff &&
+				(buffer[3] == (char)0xe0 ||
+				buffer[3] == (char)0xe1 ||
+				buffer[3] == (char)0xe8)) {
+			is_jpg = 1;
+		}
+		printf("is_jpg: %d, inode num is [%d]\n", is_jpg, i );
+			
+		char* inode_num = malloc(2); 
+		inode_num = "13"; // for for test, need to be modified.
+
+		// only test inode13, need to be modified.
+		if (is_jpg && i == 13) 
+		{ 
+			for(int j = 0; j < EXT2_N_BLOCKS; j++) 
+			{
+
+				if(inode->i_block[j] == 0) continue;
+
+				lseek(fd, (int)inode->i_block[j] * block_size, SEEK_SET);    /* position data block */
+				memset (buffer, 0, block_size);
+				read(fd, buffer, block_size);    	
+				//printf("r_value: %d\n", r_value);
+
+
+				// filename path
+				char str1[] = "./";
+				char str2[] = "/file-";
+				char str3[] = ".jpg";
+				int len = strlen(str1) + strlen(argv[2]) + strlen(str2) + strlen(inode_num) + strlen(str3) + 1;
+				char path[len];
+				memset(path, '\0', len);
+				strcat(path, str1);
+				strcat(path, argv[2]);
+				strcat(path, str2);
+				strcat(path, inode_num);
+				strcat(path, str3);
+				// write filename to file
+				FILE *fp = fopen(path, "a+"); 
+				if(fp == NULL) {
+					printf("Fail to open file.");
+					exit(0);
+				}
+				else {
+					fputs(buffer, fp);
+				}
+				fclose(fp);
 			}
-			off_t tmp;
-			if (S_ISDIR(inode->i_mode) && ((tmp = inode->i_block[i]) != 0) ) {
-				printf("Directory inode\n");
+		}
+	}
 
-			} else if (S_ISREG(inode->i_mode) && ((tmp = inode->i_block[i]) != 0)) {
-				lseek(fd, BLOCK_OFFSET(tmp), SEEK_SET); // a: 217, tmp: 217
+		// Part2: record filenames -> can output the names, but can;'t get the filename which has been deleted.
+		if (S_ISDIR(inode->i_mode)) 
+		{
+			struct ext2_dir_entry *dentry = malloc(sizeof(struct ext2_dir_entry));
+			lseek(fd, (int)inode->i_block[0] * block_size, SEEK_SET); // position data block */
+			read(fd, buffer, sizeof(struct ext2_dir_entry));
 
-				for(unsigned int j = 0; j<inodes_per_block; j++) {
-					printf("\tnext_inode %u: \n", j);
-					struct ext2_inode* next_inode = malloc(sizeof(struct ext2_inode));
-					read_inode(fd, 0, BLOCK_OFFSET(tmp), j, next_inode); 
+			int entry_offset = 0 ;
 
-					unsigned int new_i_blocks = next_inode->i_blocks/(2<<super.s_log_block_size);
-					// print inode info
-					printf("\t\tsize: %u\n",next_inode->i_size);
-					printf("\t\tnumber of blocks: %u, inode->i_blocks: %u\n", new_i_blocks, next_inode->i_blocks);
-					printf("\t\tIs directory? %s, Is Regular file? %s\n", S_ISDIR(next_inode->i_mode) ? "true" : "false", 
-										S_ISREG(next_inode->i_mode) ? "true" : "false");
-					// print inode block info
-					for(unsigned int j=0; j<EXT2_N_BLOCKS; j++) {
-						if (j < EXT2_NDIR_BLOCKS){                                 /* direct blocks */
-							printf("\t\tBlock %2u : %u\n", j, next_inode->i_block[j]);
-						} else if (j == EXT2_IND_BLOCK) {                             /* single indirect block */
-							printf("\t\tSingle   : %u\n", next_inode->i_block[j]);
-						} else if (j == EXT2_DIND_BLOCK) {                            /* double indirect block */
-							printf("\t\tDouble   : %u\n", next_inode->i_block[j]);
-						} else if (j == EXT2_TIND_BLOCK) {                            /* triple indirect block */
-							printf("\t\tTriple   : %u\n", next_inode->i_block[j]);
-						}
+			while (entry_offset != (int)block_size) 
+			{
+				dentry = (struct ext2_dir_entry*) & (buffer[0 + entry_offset]);
+
+				int name_len = dentry->name_len & 0xFF; // convert 2 bytes to 4 bytes properly
+				char name [EXT2_NAME_LEN];
+				strncpy(name, dentry->name, name_len);
+				name[name_len] = '\0';
+				entry_offset = entry_offset + dentry->rec_len;
+				struct ext2_inode *inode_check = malloc(sizeof(struct ext2_inode));
+				read_inode(fd, 0, start_inode_table, dentry->inode, inode_check);
+				
+				if (S_ISREG(inode_check->i_mode)) 
+				{
+					// filename path
+					char str1[] = "./";
+					char str2[] = "/filename.txt";
+					int len = strlen(str1) + strlen(argv[2]) + strlen(str2) + 1;
+					char path[len];
+					memset(path, '\0', len);
+					strcat(path, str1);
+					strcat(path, argv[2]);
+					strcat(path, str2);
+					// write filename to file
+					FILE *fp = fopen(path, "a+"); 
+					if(fp == NULL) {
+						printf("Fail to open file.");
+						exit(0);
 					}
+					else {
+						fputs(name, fp);
+						fputs("\n", fp);
+					}
+					fclose(fp);
+					
+					printf("Entry name is --%s--; Inode num is [%d]\n", name, dentry->inode);
 				}
 			}
 		}
 		
+		// print i_block numberss
+		for(unsigned int i=0; i<EXT2_N_BLOCKS; i++)
+		{       if (i < EXT2_NDIR_BLOCKS)                                 /* direct blocks */
+						printf("Block %2u : %u\n", i, inode->i_block[i]);
+				else if (i == EXT2_IND_BLOCK)                             /* single indirect block */
+						printf("Single   : %u\n", inode->i_block[i]);
+				else if (i == EXT2_DIND_BLOCK)                            /* double indirect block */
+						printf("Double   : %u\n", inode->i_block[i]);
+				else if (i == EXT2_TIND_BLOCK)                            /* triple indirect block */
+						printf("Triple   : %u\n", inode->i_block[i]);
+		}
 		free(inode);
-
-	}
-
-	
+	}		
 	close(fd);
 }
